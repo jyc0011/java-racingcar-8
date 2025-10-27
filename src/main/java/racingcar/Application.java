6688d13d3f86e67a6badc6ee8997af0e2161d4cd
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-10-27 15:30:50 +0900

    docs: add javaDocs
    add javaDocs about Applcation.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RaceGameController;
+
+/**
+ * 애플리케이션 Entry Point
+ * <p>
+ * {@code main} 메서드를 포함하며, {@link RaceGameController} 생성, 실행
+ */
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceGameController gameController = new RaceGameController();
+        gameController.run();
     }
 }
